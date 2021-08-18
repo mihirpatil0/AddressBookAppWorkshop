01303a949d22685aa2f848620eb64b3ef0d7363b
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-18 16:21:18 +0530

    [mihir] Refactored: changed file name and added button function.

---
+++ js/ContactForm.js
@@ -0,0 +1,91 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const name = document.querySelector("#name");
+    const nameError = document.querySelector(".name-error");
+    name.addEventListener("input", function () {
+        if (name.value.length == 0) {
+            nameError.textContent = "";
+            return;
+        }
+        try {
+            new Contact().name = name.value;
+            nameError.textContent = "";
+        } catch (error) {
+            nameError.textContent = error;
+        }
+    });
+
+    const phoneNumber = document.querySelector("#phoneNumber");
+    const numberError = document.querySelector(".tel-error");
+    phoneNumber.addEventListener("input", function () {
+        if (phoneNumber.value.length == 0) {
+            numberError.textContent = "";
+            return;
+        }
+        try {
+            new Contact().phoneNumber = phoneNumber.value;
+            numberError.textContent = "";
+        } catch (error) {
+            numberError.textContent = error;
+        }
+    });
+
+    const zip = document.querySelector("#zip");
+    const zipError = document.querySelector(".zip-error");
+    zip.addEventListener("input", function () {
+        if (zip.value.length == 0) {
+            zipError.textContent = "";
+            return;
+        }
+        try {
+            new Contact().zip = zip.value;
+            zipError.textContent = "";
+        } catch (error) {
+            zipError.textContent = error;
+        }
+    });
+});
+
+function save() {
+    let contact = new Contact()
+    contact.id = new Date().getTime()
+    try {
+        contact.name = getInputValueById("#name");
+    } catch (error) {
+        setTextValue(".name-error", error);
+        throw error;
+    }
+
+    try {
+        contact.phoneNumber = getInputValueById("#phoneNumber");
+    } catch (error) {
+        setTextValue(".tel-error", error);
+        throw error;
+    }
+    contact.address = getInputValueById("#address");
+    let city = getInputValueById("#city");
+    if (city != "Select City") {
+        contact.city = city;
+    } else {
+        throw "Please select city";
+    }
+    let state = getInputValueById("#state");
+    if (state != "Select State") {
+        contact.state = state;
+    } else {
+        throw "Please select state";
+    }
+
+    try {
+        contact.zip = getInputValueById("#zip");
+    } catch (error) {
+        setTextValue(".zip-error", error);
+        throw error;
+    }
+
+    alert(contact.toString());
+}
+
+function getInputValueById(property) {
+    let value = document.querySelector(property).value;
+    return value;
+}
