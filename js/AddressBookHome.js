424ca83f702cd3dc7e318676087333c01e531309
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-18 17:42:43 +0530

    [mihir] Added: function to edit contact details.

--- js/AddressBookHome.js
+++ js/AddressBookHome.js
@@ -1,3 +1,4 @@
+let contactList
 window.addEventListener("DOMContentLoaded", (event) => {
     contactList = getContactFromStorage()
     document.querySelector(".contact-count").textContent = contactList.length;
@@ -14,30 +15,30 @@ const createInnerHtml = () => {
         return;
     }
     const headerHtml = `<tr>
-      <th>Name</th>
-      <th>Address</th>
-      <th>City</th>
-      <th>State</th>
-      <th>Zip Code</th>
-      <th>Phone Number</th>
-      </tr>`;
+    <th>Name</th>
+    <th>Address</th>
+    <th>City</th>
+    <th>State</th>
+    <th>Zip Code</th>
+    <th>Phone Number</th>
+    </tr>`;
     let innerHtml = `${headerHtml}`;
     for (const contact of contactList) {
         innerHtml = `${innerHtml} 
-          <tr>
-          <td>${contact._name}</td>
-          <td>
-              ${contact._address}
-          </td>
-          <td>${contact._city}</td>
-          <td>${contact._state}</td>
-          <td>${contact._zip}</td>
-          <td>${contact._phoneNumber}</td>
-          <td>
-              <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="${contact._id}" onclick="remove(this)">
-              <img src="../assets/icons/create-black-18dp.svg" alt="update" id="${contact._id}" onclick="update(this)">
-          </td>
-          </tr>`;
+        <tr>
+        <td>${contact._name}</td>
+        <td>
+            ${contact._address}
+        </td>
+        <td>${contact._city}</td>
+        <td>${contact._state}</td>
+        <td>${contact._zip}</td>
+        <td>${contact._phoneNumber}</td>
+        <td>
+            <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="${contact._id}" onclick="remove(this)">
+            <img src="../assets/icons/create-black-18dp.svg" alt="update" id="${contact._id}" onclick="update(this)">
+        </td>
+        </tr>`;
     }
     document.querySelector("#table-display").innerHTML = innerHtml;
 };
@@ -52,4 +53,13 @@ function remove(node) {
     localStorage.setItem("ContactList", JSON.stringify(contactList))
     document.querySelector(".contact-count").textContent = contactList.length
     createInnerHtml();
+}
+
+function update(node) {
+    let contactEdit = contactList.find(editContact => editContact._id == node.id)
+    if (!contactEdit) {
+        return
+    }
+    localStorage.setItem('contactEdit', JSON.stringify(contactEdit))
+    window.location.replace("../pages/AddressBookFrom.html")
 }
